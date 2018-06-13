6065ba06a6f62da809bd761962b14a84d8af15b7
Arif Jusoh <arifjusoh@gmail.com>
2018-06-13 21:07:53 +0800

    added swift example

---
+++ eGHL Example (swift 3)/eGHLSwiftExample/BridgingForObjC.h
@@ -0,0 +1,14 @@
+//
+//  BridgingForObjC.h
+//  eGHLSwiftExample
+//
+//  Created by Arif Jusoh on 13/03/2017.
+//  Copyright © 2017 GHL EPayments. All rights reserved.
+//
+
+#ifndef BridgingForObjC_h
+#define BridgingForObjC_h
+
+#import <EGHL/EGHL.h>
+
+#endif /* BridgingForObjC_h */
